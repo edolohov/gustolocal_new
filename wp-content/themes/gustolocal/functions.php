e12d0a88e70b8bda094eb2c10f62b43dd8417751
edolohov <edolohov@gmail.com>
2025-11-10 17:26:13 +0100

    Fix mobile menu: override width and height for is-open state

--- wp-content/themes/gustolocal/functions.php
+++ wp-content/themes/gustolocal/functions.php
@@ -4,7 +4,7 @@
  */
 
 if ( ! defined( 'GUSTOLOCAL_VERSION' ) ) {
-    define( 'GUSTOLOCAL_VERSION', '0.3.7' );
+    define( 'GUSTOLOCAL_VERSION', '0.3.8' );
 }
 
 add_action( 'after_setup_theme', function () {
