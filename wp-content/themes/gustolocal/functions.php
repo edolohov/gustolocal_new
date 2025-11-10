5eae24d79392c5ecca85148cad9c35149be561cc
edolohov <edolohov@gmail.com>
2025-11-10 18:16:01 +0100

    Add rico page: styles, template, gallery script

--- wp-content/themes/gustolocal/functions.php
+++ wp-content/themes/gustolocal/functions.php
@@ -4,7 +4,7 @@
  */
 
 if ( ! defined( 'GUSTOLOCAL_VERSION' ) ) {
-    define( 'GUSTOLOCAL_VERSION', '0.4.4' );
+    define( 'GUSTOLOCAL_VERSION', '0.5.0' );
 }
 
 add_action( 'after_setup_theme', function () {
@@ -29,6 +29,11 @@ add_action( 'wp_enqueue_scripts', function () {
     $theme_dir = get_template_directory_uri();
     wp_enqueue_style( 'gustolocal-main', $theme_dir . '/style.css', [], GUSTOLOCAL_VERSION );
     wp_enqueue_script( 'gustolocal-navigation', $theme_dir . '/assets/js/navigation.js', [], GUSTOLOCAL_VERSION, true );
+    
+    // Load gallery script only on rico page
+    if ( is_page( 'rico' ) || ( is_page() && get_post_field( 'post_name' ) === 'rico' ) ) {
+        wp_enqueue_script( 'gustolocal-rico-gallery', $theme_dir . '/assets/js/rico-gallery.js', [], GUSTOLOCAL_VERSION, true );
+    }
 } );
 
 add_action( 'enqueue_block_editor_assets', function () {
