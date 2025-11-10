6e75c0183d4f7f1e8e7759fcc759813d10487236
edolohov <edolohov@gmail.com>
2025-11-10 17:04:42 +0100

    Fix mobile header: center LLÉVATELO title instead of left-align

--- wp-content/themes/gustolocal/functions.php
+++ wp-content/themes/gustolocal/functions.php
@@ -4,7 +4,7 @@
  */
 
 if ( ! defined( 'GUSTOLOCAL_VERSION' ) ) {
-    define( 'GUSTOLOCAL_VERSION', '0.3.2' );
+    define( 'GUSTOLOCAL_VERSION', '0.3.3' );
 }
 
 add_action( 'after_setup_theme', function () {
